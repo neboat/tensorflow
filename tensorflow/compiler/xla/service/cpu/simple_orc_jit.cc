94d464dd592a2fb5628fbf88be4c807e7acc83ce
banex19 <vettoreldaniele@gmail.com>
2019-03-15 20:00:55 -0400

    Support for CSI. The CSI constructor is now called after every module compilation.

--- tensorflow/compiler/xla/service/cpu/simple_orc_jit.cc
+++ tensorflow/compiler/xla/service/cpu/simple_orc_jit.cc
@@ -98,8 +98,7 @@ SimpleOrcJIT::SimpleOrcJIT(const llvm::TargetOptions& target_options,
                            bool disable_expensive_passes,
                            LLVMCompiler::ModuleHook pre_optimization_hook,
                            LLVMCompiler::ModuleHook post_optimization_hook,
-                           bool run_cilksan,
-                           bool run_csi)
+                           bool run_cilksan, bool run_csi)
     : target_machine_(InferTargetMachineForJIT(target_options, opt_level)),
       disassembler_(*target_machine_),
       data_layout_(target_machine_->createDataLayout()),
@@ -126,22 +125,33 @@ SimpleOrcJIT::SimpleOrcJIT(const llvm::TargetOptions& target_options,
                                      enable_fast_math, disable_expensive_passes,
                                      std::move(pre_optimization_hook),
                                      std::move(post_optimization_hook),
-                                     run_cilksan,
-                                     run_csi)) {
+                                     run_cilksan, run_csi)) {
   VLOG(1) << "CPU target: " << target_machine_->getTargetCPU().str()
           << " features: " << target_machine_->getTargetFeatureString().str();
   string error;
   auto dy_lib =
-    llvm::sys::DynamicLibrary::getPermanentLibrary("libcilkrts.so.5", &error);
+      llvm::sys::DynamicLibrary::getPermanentLibrary("libcilkrts.so.5", &error);
   if (!dy_lib.isValid())
     VLOG(1) << "Error loading Cilk runtime system: " << error << "\n";
 
   if (run_cilksan) {
-    dy_lib =
-      llvm::sys::DynamicLibrary::getPermanentLibrary(
-          "libclang_rt.cilksan-x86_64.so", &error);
-    if (!dy_lib.isValid())
+    dy_lib = llvm::sys::DynamicLibrary::getPermanentLibrary(
+        "libclang_rt.cilksan-x86_64.so",
+        &error);
+    if (!dy_lib.isValid()) {
       VLOG(1) << "Error loading Cilksan library: " << error << "\n";
+      exit(-1);
+    }
+  }
+
+  if (run_csi) {
+    dy_lib = llvm::sys::DynamicLibrary::getPermanentLibrary(
+        "libclang_rt.csi-x86_64.so",
+        &error);
+    if (!dy_lib.isValid()) {
+      VLOG(1) << "Error loading Cilksan library: " << error << "\n";
+      exit(-1);
+    }
   }
 
   llvm::sys::DynamicLibrary::LoadLibraryPermanently(nullptr);
